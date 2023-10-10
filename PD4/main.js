9c0a7c5df6f0ba8d121eebe1c0ee83128a42daa2
Neda <98neda@gmail.com>
2023-10-10 12:30:36 +0300

    updeitai

---
+++ PD4/main.js
@@ -0,0 +1,11 @@
+let petName = "Amsis";
+let petAge = 2;
+
+console.log(petName, petAge);
+
+petName = "Amsis";
+petAge = 2;
+
+const buildingAddress = "308 Negra Arroyo Lane";
+
+console.log(buildingAddress);
---
+++ PD5/main.js
@@ -0,0 +1,57 @@
+let numberType = 5;
+console.log(typeof numberType);
+
+let bigIntType = 41n;
+console.log(typeof bigIntType);
+
+let stringType = ("Hello World!");
+console.log(typeof stringType);
+
+let booleanType = true;
+console.log(typeof booleanType);
+
+let undefinedType;
+console.log(typeof undefinedType);
+
+let nullType = {};
+nullType = null;
+console.log(nullType);
+
+let objectType = {};
+console.log(typeof objectType);
+
+
+
+
+let number = 12;
+console.log(typeof number);
+
+let otherNumber = 45.89;
+console.log(typeof otherNumber);
+
+let textOne = "Beautiful Javascript World";
+console.log(typeof textOne);
+
+let textTwo = "Beautiful JavaScript World";
+console.log(typeof textTwo);
+
+let textThree = "Beautiful JavaScript World";
+console.log(typeof textThree);
+
+let isTrue = (true == false);
+console.log (typeof isTrue);
+
+let isEqual = (2 >= 0);
+console.log(typeof isEqual);
+
+let petName = undefined;
+console.log(typeof petName);
+
+let petAge = null;
+console.log(typeof petAge);
+
+let human = {
+    humanName: undefined,
+    humanAge: null
+};
+console.log (typeof human);
