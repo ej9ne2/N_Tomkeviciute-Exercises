38c1b6097a04377fae4e1d239f7084b4c4af9b27
Neda <98neda@gmail.com>
2023-11-20 11:26:30 +0200

    new updetos

---
+++ PD6/js/main.js
@@ -0,0 +1,11 @@
+let num = 12345;
+let text = num.toString();
+
+console.log(typeof(text))
+
+let funnyString = "12345"
+console.log(typeof(Number(funnyString)))
+
+let funnyTrue = "1"
+console.log(typeof (Boolean(funnyTrue)))
+
